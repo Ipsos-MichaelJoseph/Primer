6f5e831655c28d6c15c4973b2b0995699a00c0fb
Michael Joseph <Michael.Joseph@ipsos.com>
2025-06-15 01:22:02 +0100

    Updated GetPageSize to async

--- Primer/Controllers/PageSizeController.cs
+++ Primer/Controllers/PageSizeController.cs
@@ -4,19 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Primer.Controllers
 {
     public class PageSizeController : ApiController
     {
-        private static string TargetUrl = "http://apress.com";
+        private static string TargetUrl = "http://neverssl.com"; // "https://www.apress.com/gb";
 
-        public long GetPageSize()
+        public async Task<long> GetPageSize()
         {
             WebClient wc = new WebClient();
             Stopwatch sw = Stopwatch.StartNew();
-            byte[] apressData = wc.DownloadData(TargetUrl);
+            byte[] apressData = await wc.DownloadDataTaskAsync(TargetUrl);
+
             Debug.WriteLine("Ellapsed Time {0} ms", sw.ElapsedMilliseconds);
 
             return apressData.LongLength;
